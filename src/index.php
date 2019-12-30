097dfc54c3d12f8a06a9398f605f76855c3b1d4b
zgq354 <zgq354@gmail.com>
2019-12-30 23:30:04 +0800

    fix: remove 404

--- src/index.php
+++ src/index.php
@@ -9,8 +9,8 @@ preg_match("/^([0-9-]*)?\.jpg\??.*?$/i", $args[2], $matches);
 
 // format
 if ($args[1] !== 'cover' || !$matches) {
-  http_response_code(404);
-  die;
+  header("Location: /assets/book-default.jpg");
+  exit;
 }
 
 $isbn = $matches[1];
@@ -21,24 +21,34 @@ if (file_exists($path)) {
   $imgData = file_get_contents($path);
   header("Content-Type: image/jpeg");
   echo $imgData;
+  exit;
 }
 
 //
 // fetch remote
 $url = "http://book.douban.com/isbn/$isbn/";
-$data = file_get_contents($url);
-$regex = '/<a class="nbg"\s*?href="(.+?)"[\s\S]*?<img src="(.*?)"/i';
+$data = @file_get_contents($url);
 
-preg_match($regex, $data, $matches);
-$imgLink = $matches[1];
-if (strpos($imgLink, "update_image") > -1) {
-  header("Location: /assets/book-default.jpg");
-} else {
-  // use small img
-  $imgLink = str_replace('/l/', '/s/', $imgLink);
-  $imgData = file_get_contents($imgLink);
-  file_put_contents($path, $imgData);
-  header("Content-Type: image/jpeg");
-  header("Cache-Control: 2592000");
-  echo $imgData;
+if ($data) {
+  $regex = '/<a class="nbg"\s*?href="(.+?)"[\s\S]*?<img src="(.*?)"/i';
+  preg_match($regex, $data, $matches);
+
+  if (!empty($matches)) {
+    $imgLink = $matches[1];
+
+    if (strpos($imgLink, "update_image") === false) {
+      // use small img
+      $imgLink = str_replace('/l/', '/s/', $imgLink);
+      $imgData = file_get_contents($imgLink);
+      file_put_contents($path, $imgData);
+
+      // echo
+      header("Content-Type: image/jpeg");
+      header("Cache-Control: 2592000");
+      echo $imgData;
+      exit;
+    }
+  }
 }
+
+header("Location: /assets/book-default.jpg");
